fa01a7a80ade03ae3a7f09c65d55740543dae276
FNahuelO <fosorio@aidforaids.org>
2023-10-02 22:17:39 -0300

    fix: remove-.env

--- src/helpers/actions.js
+++ src/helpers/actions.js
@@ -1,7 +1,7 @@
 import { baseURl } from "../axios";
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_YOUTUBE_KEY;
+const API_KEY = process.env.REACT_APP_GOOGLE_KEY;
 
 export const getInvitados = async () => {
   try {
