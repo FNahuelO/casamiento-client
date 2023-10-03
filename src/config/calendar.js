59389117560a1d711bb30c993b12ee8811386b76
FNahuelO <fosorio@aidforaids.org>
2023-10-02 21:54:51 -0300

    feat: master-page

--- src/config/calendar.js
+++ src/config/calendar.js
@@ -3,7 +3,7 @@ import ApiCalendar from "react-google-calendar-api";
 const config = {
   clientId:
     "222984064814-e7803mbvv20pu74vnr7con3sl1kofd38.apps.googleusercontent.com",
-  apiKey: "AIzaSyDpesVOLEmp857aWlYTUAAAMOP_ZN8g9m0",
+  apiKey: process.env.REACT_APP_GOOGLE_KEY,
   scope: "https://www.googleapis.com/auth/calendar",
   discoveryDocs: [
     "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
